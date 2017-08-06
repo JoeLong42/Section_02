51d9e7d25055b8fb92157bc6608bd31941808b47
Joseph <smoispwns@gmail.com>
2017-08-06 20:12:15 +0900

    Commit 2

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -8,8 +8,20 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+//dont use more than one "using namespace" call
+using namespace std;
+
+int main(void) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    constexpr int WORD_LENGTH = 0;
+    
+    cout << "Hello, World!\n";
+    cout << "Enter your name: ";
+    string name = "";
+    cin >> name;
+    cout << name << std::endl;
+    
+    cout << "The length of the isogram is " << WORD_LENGTH << " letters long." << endl;
+    
     return 0;
 }
