37deca2956dbdae5acd5b554afae8a6abff3155c
Joseph <smoispwns@gmail.com>
2017-08-06 19:29:15 +0900

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Joseph Long on 8/6/17.
+//  Copyright © 2017 Joseph Long. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
